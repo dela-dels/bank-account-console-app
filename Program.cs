10735e84510c63ba71c4c490767d07ac6bff85e5
Dela Akakpo <delprane14@gmail.com>
2021-12-29 16:10:34 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankAccountConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Kindly enter your name");
+            // string clientName = Console.ReadLine();
+
+            // Console.WriteLine("Enter your initial account balance.");
+            // // We are using the Convert.ToDecimal here because the Console.ReadLine
+            // // method only returns strings and hence, to use any other type, we will have to
+            // // perform some casting
+            // decimal initialAccountDeposit = Convert.ToDecimal(Console.ReadLine());
+
+            //After taking the inputs from the user, we go ahead to create a new accoutn for them
+            // by building a new BankAccount Object
+            var account = new BankAccount("John Doe", 10000);
+            Console.WriteLine($"Your Account with number {account.Number} was created with an initial balance of {account.Balance}");
+            account.Withdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.Deposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.TransactionHistory());
+        }
+    }
+}
---
+++ classes/BankAccount.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+
+namespace BankAccountConsole
+{
+    public class BankAccount
+    {
+        private static int accountNumberSeeder = 0100000001;
+
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+
+                foreach (var item in transactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        public string Owner { get; }
+
+        public string Number { get; }
+
+        public BankAccount(string name, decimal initialDeposit)
+        {
+            this.Owner = name;
+            Deposit(initialDeposit, DateTime.Now, "Initial Deposit");
+            this.Number = accountNumberSeeder.ToString();
+            accountNumberSeeder++;
+        }
+
+        private List<Transaction> transactions = new List<Transaction>();
+
+        public void Deposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+
+            var deposit = new Transaction(amount, date, note);
+            transactions.Add(deposit);
+        }
+
+        public void Withdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Withdrawal amount must be positive");
+            }
+
+            if ((Balance - amount) < 0)
+            {
+                throw new InvalidOperationException("Insufficient funds for this withdrawal");
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            transactions.Add(withdrawal);
+        }
+
+        public string TransactionHistory()
+        {
+            var report = new System.Text.StringBuilder();
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+
+            foreach (var item in transactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
